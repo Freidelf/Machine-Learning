3b529250af265c84011187beb67daf89b750c1d2
Freidelf <markus.grawe@gmail.com>
2019-03-04 18:43:49 +0100

    Added code to generate a number of multivariate gaussian distributed samples similarly to the description in chapter two of "The elements of statistical learning"

--- Data-Generation/GenerateData.py
+++ Data-Generation/GenerateData.py
@@ -1,5 +1,30 @@
+from pprint import pformat
 import numpy as np
 
 
-def gaussianData(mode, variables = 1, mean, std, ys):
-    
+def gaussianData(poolSize,
+                 Observations,
+                 mean,
+                 std = 0):
+    variables = len(mean)
+    std = np.eye(variables)
+    meanPool = np.random.multivariate_normal(mean,std,poolSize)
+    print(meanPool)
+    ObsList = [[0]*variables] * Observations
+    for i in range(Observations):
+        if poolSize > 1:
+            r = np.random.randint(0,poolSize)
+        else:
+            r = 0
+        ObsList[i] = (np.random.multivariate_normal(
+            meanPool[r],
+            np.multiply(std,0.2)),
+                      meanPool[r])
+    return ObsList
+
+def main():
+    print(gaussianData(3,10,[1,0]))
+
+if __name__ == '__main__':
+    main()
+        
