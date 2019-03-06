d61939c884b762c0a9295a0e3fa668398807e891
Freidelf <markus.grawe@gmail.com>
2019-03-06 17:22:46 +0100

    Wrote code to add data into a kD-tree, and also code to use the kD-tree to quickly generate nearest neighbour searches.

--- GenerateData.py
+++ GenerateData.py
@@ -5,25 +5,41 @@ import numpy as np
 def gaussianData(poolSize,
                  Observations,
                  mean,
+                 yVal,
                  std = 0):
     variables = len(mean)
     std = np.eye(variables)
     meanPool = np.random.multivariate_normal(mean,std,poolSize)
-    ObsList = np.ndarray(shape = (Observations, variables))    
+    ObsList = np.ndarray(shape = (Observations, variables+1))    
     for i in range(Observations):
         r = np.random.randint(0,poolSize)
         temp = np.random.multivariate_normal(
             meanPool[r],
             np.multiply(std,0.2)).tolist()
-        ObsList[i,:] = temp
+        ObsList[i,:variables] = temp
+        ObsList[i,variables] = yVal
     return [ObsList, meanPool]
 
+
+        
+def domainFillData2D(ranges):
+    outData = np.ndarray(shape = (10000,3),dtype = float)
+    X1_range = ranges[1]-ranges[0]
+    outData[:,0] = np.tile(ranges[0] + range(100)*X1_range/100,100)
+    X2_vals = np.linspace(ranges[2],ranges[3],100)
+    for i in range(100):
+        outData[range(i*100,(i+1)*100),1] = np.linspace(X2_vals[i],X2_vals[i],100)
+    return outData
+
+        
+
 def main():
-    a = gaussianData(2,5,[1,0])
+    a = gaussianData(2,5,[1,0],1)
     print(a[0])
     print("******")
     print(a[1])
 
+        
+
 if __name__ == '__main__':
     main()
-        
---
+++ kap2-KNN.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import kap2_kdTree2D
+import GenerateData
+from collections import namedtuple
+from operator import itemgetter
+from pprint import pformat
+import math
+
+def distance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+
+    dx = x1 - x2
+    dy = y1 - y2
+
+    return math.sqrt(dx*dx + dy*dy)
+
+def closerDist(pivot, p1, p2):
+    if p1 is None:
+        return p2
+    if p2 is None:
+        return p1
+    
+    d1 = distance(pivot[0:2],p1[0:2])
+    d2 = distance(pivot[0:2],p2[0:2])
+    
+    if d1 < d2:
+        return p1
+    else:
+        return p2
+    
+def oneNearestPoint(root,point,depth = 0):
+    if root is None:
+        return None
+
+    axis = depth % 2
+
+    next_branch = None
+    opposite_branch = None
+   
+    if point[axis] < root[0][axis]:
+        next_branch = root[1]
+        opposite_branch = root[2]
+    else:
+        next_branch = root[2]
+        opposite_branch = root[1]
+        
+    best = closerDist(point,
+                          oneNearestPoint(
+                              next_branch,
+                              point,
+                              depth + 1),
+                          root[0])
+    
+    if distance(point[0:2], best[0:2]) > abs(point[axis] -  root[0][axis]):
+        best = closerDist(point,
+                              oneNearestPoint(
+                                  opposite_branch,
+                                  point,
+                                  depth + 1),
+                              best)
+    return best
+
+
+def main():
+    obs1 = GenerateData.gaussianData(10,100,[0,1],0)[0]
+    obs2 = GenerateData.gaussianData(10,100,[1,0],1)[0]
+    min1 = min(min(obs1[:,0]),min(obs2[:,0]))
+    max1 = max(max(obs1[:,0]),max(obs2[:,0]))
+    min2 = min(min(obs1[:,1]),min(obs2[:,1]))
+    max2 = max(max(obs1[:,1]),max(obs2[:,1]))
+    obs11 = obs1.tolist()
+    obs22 = obs2.tolist()
+    X = []
+    for i in range(100):
+        X.append((obs1[i,0],obs1[i,1],0))
+    for i in range(100):
+        X.append((obs2[i,0],obs2[i,1],1))
+    tree = kap2_kdTree2D.buildTree(X)
+    domainRanges = [min1,max1,min2,max2]
+    fillData = GenerateData.domainFillData2D(domainRanges)
+    for i in range(len(fillData[:,0])):
+        point = (fillData[i,0], fillData[i,1])
+        t = oneNearestPoint(tree,point)
+        fillData[i,2] = t[2]
+    plt.plot(obs1[:,0],obs1[:,1], 'ro')
+    plt.plot(obs2[:,0],obs2[:,1], 'bo')
+    plt.scatter(fillData[:,0],fillData[:,1], c = fillData[:,2], s = 0.3)
+    plt.show()
+if __name__ == '__main__':
+    main()
